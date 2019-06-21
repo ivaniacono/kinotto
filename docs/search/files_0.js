598e5f46b1e3609a5472c2ce099d5cc414bbc6f3
Ivan Iacono <ivan.iacono@gmail.com>
2019-06-21 18:45:55 +0100

    update doc

--- docs/search/files_0.js
+++ docs/search/files_0.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['kinotto_5fip_5futils_2eh',['kinotto_ip_utils.h',['../kinotto__ip__utils_8h.html',1,'']]],
-  ['kinotto_5fjson_5futils_2eh',['kinotto_json_utils.h',['../kinotto__json__utils_8h.html',1,'']]],
+  ['kinotto_5fif_2eh',['kinotto_if.h',['../kinotto__if_8h.html',1,'']]],
+  ['kinotto_5fjson_2eh',['kinotto_json.h',['../kinotto__json_8h.html',1,'']]],
+  ['kinotto_5fnet_2eh',['kinotto_net.h',['../kinotto__net_8h.html',1,'']]],
   ['kinotto_5ftypes_2eh',['kinotto_types.h',['../kinotto__types_8h.html',1,'']]],
   ['kinotto_5fwifi_5fsta_2eh',['kinotto_wifi_sta.h',['../kinotto__wifi__sta_8h.html',1,'']]],
   ['kinotto_5fwifi_5fsta_5ftypes_2eh',['kinotto_wifi_sta_types.h',['../kinotto__wifi__sta__types_8h.html',1,'']]],
